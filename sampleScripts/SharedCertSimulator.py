cbf91f45ff84c93911baef6cc90c0bfc62a32054
Syed Rehan <64199676+redmancodes@users.noreply.github.com>
2021-02-18 00:09:33 +0000

    Add files via upload
    added sample scripts for simulation

---
+++ sampleScripts/SharedCertSimulator.py
@@ -0,0 +1,108 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0.
+# This script is for demonstration purposes only and not intended for production use.
+
+import os.path
+import sys
+import time
+from uuid import uuid4
+import json
+import argparse
+import boto3
+
+from awscrt import io
+from awsiot import mqtt_connection_builder
+
+policy_doc = {
+    "Version": "2012-10-17",
+    "Statement": [{"Effect": "Allow", "Action": ["iot:Connect"], "Resource": ["*"]}],
+}
+
+
+def connect_me(endpoint, cert, client_bootstrap, thing_name):
+    mqtt_connection = mqtt_connection_builder.mtls_from_bytes(
+        endpoint=endpoint,
+        cert_bytes=cert["certificatePem"].encode("utf-8"),
+        pri_key_bytes=cert["keyPair"]["PrivateKey"].encode("utf-8"),
+        client_bootstrap=client_bootstrap,
+        ca_filepath="./AmazonRootCA1.pem",
+        client_id=thing_name,
+        keep_alive_secs=5,
+    )
+
+    print("Connecting to {} with client ID '{}'...".format(endpoint, thing_name))
+    connect_future = mqtt_connection.connect()
+    # print(connect_future)
+    connect_future.result()
+    # print(connect_future)
+
+    print(f"Connected! using {thing_name}")
+    return mqtt_connection
+
+
+def main(endpoint, region_name, aws_profile=None):
+    rand_str = str(uuid4())
+    print(rand_str)
+    if aws_profile != "default":
+        boto3.setup_default_session(profile_name=aws_profile)
+    client = boto3.client("iot", region_name=region_name)
+    cert = client.create_keys_and_certificate(setAsActive=True)
+    cert_arn = cert["certificateArn"]
+    cert_id = cert["certificateId"]
+    # print(cert_id)
+
+    thing_name1 = f"thing_name1_{rand_str}"
+
+    client.create_thing(thingName=thing_name1)
+    client.attach_thing_principal(thingName=thing_name1, principal=cert_arn)
+
+    thing_name2 = f"thing_name2_{rand_str}"
+    client.create_thing(thingName=thing_name2)
+    client.attach_thing_principal(thingName=thing_name2, principal=cert_arn)
+
+    group_name = f"tmp_group_{rand_str}"
+    thing_group = client.create_thing_group(thingGroupName=group_name)
+
+    policy_json = json.dumps(policy_doc)
+    policy_name = f"tmp_policy_{rand_str}"
+    client.create_policy(policyName=policy_name, policyDocument=policy_json)
+    client.attach_policy(policyName=policy_name, target=thing_group["thingGroupArn"])
+    client.add_thing_to_thing_group(thingGroupName=group_name, thingName=thing_name1)
+    client.add_thing_to_thing_group(thingGroupName=group_name, thingName=thing_name2)
+    event_loop_group = io.EventLoopGroup(1)
+    host_resolver = io.DefaultHostResolver(event_loop_group)
+    client_bootstrap = io.ClientBootstrap(event_loop_group, host_resolver)
+
+    connection_1 = connect_me(endpoint, cert, client_bootstrap, thing_name1)
+    print(f"Connection 1: {connection_1}")
+    connection_2 = connect_me(endpoint, cert, client_bootstrap, thing_name2)
+    print(f"Connection 2: {connection_2}")
+    with open("CLEANUP_cert.txt", "w") as f:
+        f.write(f"Certificate: {cert_id}\n")
+        f.write(f"ThingName: {thing_name1}\n")
+        f.write(f"ThingName: {thing_name2}\n")
+        f.write(f"Policy: {policy_name}\n")
+        f.write(f"GroupNames: {group_name}\n")
+
+    print("Sleeping for 10 seconds")
+    time.sleep(10)
+
+
+if __name__ == "__main__":
+    if not os.path.isfile("AmazonRootCA1.pem"):
+        print(
+            "Cannot find CA cert. Please download from: https://www.amazontrust.com/repository/AmazonRootCA1.pem"
+        )
+        sys.exit(1)
+    parser = argparse.ArgumentParser(
+        description="Create MQTT connection from two (simulated) devices with a shared certificate"
+    )
+    parser.add_argument("--aws_profile", help="AWS profile name")
+    parser.add_argument(
+        "--endpoint",
+        required=True,
+        help="AWS IoT endpoint for creating the MQTT connection",
+    )
+    parser.add_argument("--region", required=True, help="prefered AWS region")
+    args = parser.parse_args()
+    main(args.endpoint, args.region, args.aws_profile)
---
+++ sampleScripts/anom_simulator.py
@@ -0,0 +1,298 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# This script is for demonstration purposes only and not intended for production use.
+
+#>> args to run: python3 anom_simulator.py --msg-size 2000 --count 5000 --sleep-time 0.25 --thing-name anom_thing_1 --group-name anom_group --policy allow_iot_operations --security-profile-name ML_Detect_profile --region eu-west-1 --account_id <account-id> aws_profile <aws-profile-name>
+
+
+
+import os.path
+import sys
+import threading
+import time
+from uuid import uuid4
+import json
+import argparse
+import boto3
+from awscrt import io, mqtt
+from awsiot import mqtt_connection_builder
+
+RECEIVED_COUNT = 0
+
+
+# Callback when connection is accidentally lost.
+def on_connection_interrupted(connection, error, **kwargs):
+    print("Connection interrupted. error: {}".format(error))
+
+
+# Callback when an interrupted connection is re-established.
+def on_connection_resumed(connection, return_code, session_present, **kwargs):
+    print(
+        "Connection resumed. return_code: {} session_present: {}".format(
+            return_code, session_present
+        )
+    )
+
+    if return_code == mqtt.ConnectReturnCode.ACCEPTED and not session_present:
+        print("Session did not persist. Resubscribing to existing topics...")
+        resubscribe_future, _ = connection.resubscribe_existing_topics()
+
+        # Cannot synchronously wait for resubscribe result because we're on the connection's event-loop thread,
+        # evaluate result with a callback instead.
+        resubscribe_future.add_done_callback(on_resubscribe_complete)
+    if time.time() - start >= args.exitafter:
+        print(f"Terminating process since {args.exitafter} seconds have elapsed")
+        sys.exit(1)
+
+
+def on_resubscribe_complete(resubscribe_future):
+    resubscribe_results = resubscribe_future.result()
+    print("Resubscribe results: {}".format(resubscribe_results))
+
+    for topic, qos in resubscribe_results["topics"]:
+        if qos is None:
+            sys.exit("Server rejected resubscribe to topic: {}".format(topic))
+    if time.time() - start >= args.exitafter:
+        print(f"Terminating process since {args.exitafter} seconds have elapsed")
+        sys.exit(1)
+
+
+# Callback when the subscribed topic receives a message
+def on_message_received(topic, payload, **kwargs):
+    global RECEIVED_COUNT
+
+    print("Received message from topic '{}': {}".format(topic, len(payload)))
+    RECEIVED_COUNT += 1
+    if RECEIVED_COUNT == args.count:
+        received_all_event.set()
+    if time.time() - start >= args.exitafter:
+        print(f"Terminating process since {args.exitafter} seconds have elapsed")
+        sys.exit(1)
+
+
+def setup(
+    region,
+    account_id,
+    thing_name,
+    policy_name,
+    group_name,
+    security_profile_name,
+    aws_profile=None,
+):
+    if aws_profile:
+        session = boto3.Session(profile_name=aws_profile)
+        client = session.client("iot", region_name=region)
+    else:
+        client = boto3.client("iot", region_name=region)
+    thing = client.create_thing(thingName=thing_name)
+    print(f"Thing creation of {thing_name} complete!")
+    endpoint = client.describe_endpoint(endpointType="iot:Data-ATS")
+    endpoint_address = endpoint["endpointAddress"]
+    print(f"Found endpoint: {endpoint_address}")
+
+    policy_doc = {
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Effect": "Allow",
+                "Action": ["iot:Publish", "iot:Receive"],
+                "Resource": [f"arn:aws:iot:{region}:{account_id}:topic/test/topic"],
+            },
+            {
+                "Effect": "Allow",
+                "Action": ["iot:Subscribe"],
+                "Resource": [
+                    f"arn:aws:iot:{region}:{account_id}:topicfilter/test/topic"
+                ],
+            },
+            {
+                "Effect": "Allow",
+                "Action": ["iot:Connect"],
+                "Resource": [f"arn:aws:iot:{region}:{account_id}:client/*"],
+            },
+            {
+                "Effect": "Deny",
+                "Action": ["iot:Connect"],
+                "Resource": [f"arn:aws:iot:{region}:{account_id}:client/baddevice"],
+            },
+        ],
+    }
+    policy_json = json.dumps(policy_doc)
+    try:
+        policy = client.create_policy(
+            policyName=policy_name, policyDocument=policy_json
+        )
+        print(f"Created policy {policy_name}")
+        # res_policy = client.list_policies()
+    except client.exceptions.ResourceAlreadyExistsException:
+        print(f"Policy {policy_name} already exists.")
+
+    try:
+        thing_group = client.create_thing_group(thingGroupName=group_name)
+        client.attach_security_profile(
+            securityProfileName=security_profile_name,
+            securityProfileTargetArn=thing_group["thingGroupArn"],
+        )
+        client.attach_policy(
+            policyName=policy_name, target=thing_group["thingGroupArn"]
+        )
+
+    except client.exceptions.ResourceAlreadyExistsException:
+        print(f"Group {group_name} already exists.")
+    # res_thing_group = client.list_thing_groups()
+
+    client.add_thing_to_thing_group(thingGroupName=group_name, thingName=thing_name)
+
+    cert = client.create_keys_and_certificate(setAsActive=True)
+    cert_arn = cert["certificateArn"]
+
+    cert_id = cert["certificateId"]
+    print(cert_id)
+    with open("CLEANUP_cert.txt", "w") as f:
+        f.write(f"Certificate: {cert_id}\n")
+    client.attach_policy(policyName=policy_name, target=cert_arn)
+    # res_cert = client.list_principal_policies(principal=cert_arn)
+
+    return {
+        "endpoint": endpoint_address,
+        "priv": cert["keyPair"]["PrivateKey"],
+        "cert": cert["certificatePem"],
+        "thing_name": thing_name,
+        "cert_arn": cert_arn,
+        "thing_group": group_name,
+        "cert_id": cert_id,
+    }
+
+
+def main(rand_str):
+
+    print(f"ID {rand_str}")
+    # Spin up resources
+    event_loop_group = io.EventLoopGroup(1)
+    host_resolver = io.DefaultHostResolver(event_loop_group)
+    client_bootstrap = io.ClientBootstrap(event_loop_group, host_resolver)
+    print(f"Launching setup of profiles....")
+    setup_details = setup(
+        args.region,
+        args.account_id,
+        args.thing_name,
+        args.policy_name,
+        args.group_name,
+        args.security_profile_name,
+        args.aws_profile,
+    )
+
+    mqtt_connection = mqtt_connection_builder.mtls_from_bytes(
+        endpoint=setup_details["endpoint"],
+        cert_bytes=setup_details["cert"].encode("utf-8"),
+        pri_key_bytes=setup_details["priv"].encode("utf-8"),
+        client_bootstrap=client_bootstrap,
+        ca_filepath="./AmazonRootCA1.pem",
+        on_connection_interrupted=on_connection_interrupted,
+        on_connection_resumed=on_connection_resumed,
+        client_id=setup_details["thing_name"],
+        clean_session=False,
+        keep_alive_secs=6,
+    )
+
+    print(
+        "Connecting to {} with client ID '{}'...".format(
+            setup_details["endpoint"], setup_details["thing_name"]
+        )
+    )
+
+    connect_future = mqtt_connection.connect()
+
+    # Future.result() waits until a result is available
+    connect_future.result()
+    print("Connected!")
+    topic = "test/topic"
+    message = "A" * args.msg_size
+    print("Subscribing to topic '{}'...".format(topic))
+    subscribe_future, packet_id = mqtt_connection.subscribe(
+        topic=topic, qos=mqtt.QoS.AT_LEAST_ONCE, callback=on_message_received
+    )
+
+    subscribe_result = subscribe_future.result()
+    print("Subscribed with {}".format(str(subscribe_result["qos"])))
+    if args.count == 0:
+        print("Sending messages until program killed")
+    else:
+        print("Sending {} message(s)".format(args.count))
+    publish_count = 1
+    while publish_count <= args.count:
+        print(
+            f"Publishing message [{publish_count}/{args.count}] to topic '{topic}': {len(message)}"
+        )
+
+        mqtt_connection.publish(
+            topic=topic, payload=message, qos=mqtt.QoS.AT_LEAST_ONCE
+        )
+        print(f"Sleeping for {args.sleep_time} seconds")
+        time.sleep(args.sleep_time)
+        publish_count += 1
+        if time.time() - start >= args.exitafter:
+            print(f"Terminating process since {args.exitafter} seconds have elapsed")
+            sys.exit(1)
+    # Wait for all messages to be received.
+    # This waits forever if count was set to 0.
+    if args.count != 0 and not received_all_event.is_set():
+        print("Waiting for all messages to be received...")
+    received_all_event.wait()
+    print("{} message(s) received.".format(received_count))
+
+    # Disconnect
+    print("Disconnecting...")
+    disconnect_future = mqtt_connection.disconnect()
+    disconnect_future.result()
+    print("Disconnected!")
+
+
+if __name__ == "__main__":
+    if not os.path.isfile("AmazonRootCA1.pem"):
+        print(
+            "Cannot find CA cert. Please download from: https://www.amazontrust.com/repository/AmazonRootCA1.pem"
+        )
+        sys.exit(1)
+    rand_str = str(uuid4())
+    start = time.time()
+    parser = argparse.ArgumentParser(
+        description="Send and receive messages through an MQTT connection."
+    )
+    parser.add_argument("--aws_profile", help="AWS profile name")
+    parser.add_argument("--region", required=True, help="prefered AWS region")
+    parser.add_argument("--account_id", required=True, help="AWS account number")
+    parser.add_argument(
+        "--count",
+        default=10,
+        type=int,
+        help="Number of messages to publish/receive before exiting.",
+    )
+    parser.add_argument(
+        "--thing-name", default=f"my-thing-{rand_str}", help="Thing Name for device"
+    )
+    parser.add_argument(
+        "--group-name", default=f"my-group-name-{rand_str}", help="ThingGroup Name"
+    )
+    parser.add_argument(
+        "--policy-name", default=f"my-policy-{rand_str}", help="IoT Policy Name"
+    )
+    parser.add_argument(
+        "--security-profile-name",
+        required=True,
+        help="Security Profile name to attach the thing group to (the one with ML-detect enabled.)",
+    )
+    parser.add_argument(
+        "--msg-size", type=int, default=20000, help="Size of message in bytes"
+    )
+    parser.add_argument(
+        "--sleep-time", type=float, default=1.0, help="Interval between each message"
+    )
+    parser.add_argument(
+        "--exitafter", type=int, default=30 * 60, help="Exit process after X seconds"
+    )
+    args = parser.parse_args()
+
+    received_count = 0
+    received_all_event = threading.Event()
+
+    main(rand_str)
